29e119f4ff28f1ab7d865214b62a62974130380d
wssc-info <154631007+wssc-info@users.noreply.github.com>
2024-02-29 10:25:32 -0600

    Update wssc.js

--- wssc.js
+++ wssc.js
@@ -1,14 +1,20 @@
 console.info("Javascript Loaded");
 
-if(jQuery("#attach .v-toolbar__content .hidden-sm-and-down")[0]) {
-	console.log("its there");
-	
-	let div = document.createElement("div");
-	div.className="v-btn__content";
-	div.append( document.createTextNode("Lessons"))
-    let lessonA = document.createElement("a");
-    lessonA.href="/programs/swim-lessons-signup/";
-    lessonA.className="font-weight-medium v-btn v-btn--flat v-btn--router theme--light";
-    lessonA.append(div);
-    jQuery("#attach .v-toolbar__content .hidden-sm-and-down")[0].append(lessonA);
-}
+$(window).on('load', function() {
+	if(jQuery("#attach .v-toolbar__content .hidden-sm-and-down")[0]) {
+		console.log("its there");
+		
+		let div = document.createElement("div");
+		div.className="v-btn__content";
+		div.append( document.createTextNode("Lessons"))
+	    let lessonA = document.createElement("a");
+	    lessonA.href="/programs/swim-lessons-signup/";
+	    lessonA.className="font-weight-medium v-btn v-btn--flat v-btn--router theme--light";
+	    lessonA.append(div);
+	    jQuery("#attach .v-toolbar__content .hidden-sm-and-down")[0].append(lessonA);
+	}else{
+		console.log("not there");
+	}
+		
+
+});
